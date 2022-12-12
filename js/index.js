7b5e29871c145a0bd56fe010cac5064c95251906
Jean <nguyen.jean@me.com>
2022-12-12 15:37:44 +0100

    add css for responsive

--- js/index.js
+++ js/index.js
@@ -21,65 +21,32 @@ document.documentElement.style.setProperty(
   navigationHeight - 1 + 'px'
 );
 
-// slider 1
-const items = document.querySelectorAll('.slider-item.slider-1');
-const nbSlide = items.length;
-const nextSlide = document.querySelector('.button-next.slider-1');
-const prevSlide = document.querySelector('.button-prev.slider-1');
-let count = 0;
-
-const nextSlider = () => {
-  items[count].classList.remove('active');
-  count < nbSlide - 1 ? count++ : (count = 0);
-  items[count].classList.add('active');
-};
-
-const prevSlider = () => {
-  items[count].classList.remove('active');
-  count > 0 ? count-- : (count = nbSlide - 1);
-  items[count].classList.add('active');
-};
-
-// nextSlide.addEventListener('click', nextSlider);
-// prevSlide.addEventListener('click', prevSlider);
-
-// slider 2
-const items2 = document.querySelectorAll('.slider-item.slider-2');
-const nbSlide2 = items2.length;
-const nextSlide2 = document.querySelector('.button-next.slider-2');
-const prevSlide2 = document.querySelector('.button-prev.slider-2');
-let count2 = 0;
-
-const nextSlider2 = () => {
-  items2[count2].classList.remove('active');
-  count2 < nbSlide2 - 1 ? count2++ : (count2 = 0);
-  items2[count2].classList.add('active');
-};
-
-const prevSlider2 = () => {
-  items2[count2].classList.remove('active');
-  count2 > 0 ? count2-- : (count2 = nbSlide2 - 1);
-  items2[count2].classList.add('active');
-};
-
-// nextSlide2.addEventListener('click', nextSlider2);
-// prevSlide2.addEventListener('click', prevSlider2);
-
-// slider with Splide
+// slider with Splide for carrousel
 new Splide('#main-carousel', {
   type: 'fade',
   rewind: true,
   padding: '3rem',
-  width: '70vw',
+  width: '80vw',
+  breakpoints: {
+    800: {
+      width: '100vw',
+      padding: '0',
+    },
+  },
 }).mount();
 
-// video slide
-
+// video slide for clips
 new Splide('#video-slides', {
   type: 'fade',
   rewind: true,
   padding: '3rem',
-  width: '70vw',
+  width: '80vw',
+  breakpoints: {
+    800: {
+      padding: '0',
+      width: '100vw',
+    },
+  },
   video: {
     loop: true,
   },
