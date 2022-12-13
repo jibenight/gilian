8255a2545f9813584f357226a07dc78106b858ce
Jean <nguyen.jean@me.com>
2022-12-13 14:06:45 +0100

    add js object

--- js/index.js
+++ js/index.js
@@ -26,7 +26,7 @@ new Splide('#main-carousel', {
   type: 'fade',
   rewind: true,
   padding: '3rem',
-  width: '80vw',
+  width: '75vw',
   breakpoints: {
     800: {
       width: '100vw',
@@ -40,7 +40,7 @@ new Splide('#video-slides', {
   type: 'fade',
   rewind: true,
   padding: '3rem',
-  width: '80vw',
+  width: '75vw',
   breakpoints: {
     800: {
       padding: '0',
@@ -51,3 +51,31 @@ new Splide('#video-slides', {
     loop: true,
   },
 }).mount(window.splide.Extensions);
+
+let datas = {
+  images: [
+    ['phoenix-1-w1200.jpg', 'alt à renseigner'],
+    ['phoenix-2-w1200.jpg', 'alt à renseigner'],
+    ['phoenix-3-w1200.jpg', 'alt à renseigner'],
+    ['phoenix-4-w1200.jpg', 'alt à renseigner'],
+    ['phoenix-5-w1200.jpg', 'alt à renseigner'],
+  ],
+  video: [
+    ['url-video', 'url-cover'],
+    ['url-video', 'url-cover'],
+  ],
+};
+
+const insertDatas = document.getElementById('insert-datas');
+
+console.log(insertDatas);
+
+for (var prop in datas.images) {
+  console.log(datas.images[prop]);
+  insertDatas.insertAdjacentHTML(
+    'afterbegin',
+    `<li class="splide__slide">
+  <img src="./images/${datas.images[prop][1]}" alt="" />
+</li>`
+  );
+}
