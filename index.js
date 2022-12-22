a195dc4cd0549789a1c0111051cccc64d913a8ea
Jean <nguyen.jean@me.com>
2022-12-22 19:08:44 +0100

    add express js as a server

--- index.js
+++ index.js
@@ -1,9 +1,32 @@
-const http = require('http');
-const fs = require('fs');
+// spec for O2switch
+if (typeof PhusionPassenger !== 'undefined') {
+  PhusionPassenger.configure({ autoInstall: false });
+}
 
-const server = http.createServer((req, res) => {
-  res.writeHead(200, { 'content-type': 'text/html' });
-  fs.createReadStream('./dist/index.html').pipe(res);
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = 3000;
+let folder = '/dist';
+let error404 = true;
+
+app.use(express.static(path.join(__dirname, folder)));
+//render index.html page
+app.get('/', (request, response) => {
+  response.render('index.html');
 });
 
-server.listen();
+//error 404
+if (error404) {
+  app.use(function (request, response) {
+    response.status(404).sendFile(path.join(__dirname, folder, '404.html'));
+  });
+}
+
+if (typeof PhusionPassenger !== 'undefined') {
+  app.listen('passenger');
+} else {
+  app.listen(port, () => {
+    console.log(`App listening at http://localhost:${port}`);
+  });
+}
