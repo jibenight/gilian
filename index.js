f325af92674fe728675e16d77c01e72b924dce56
Jean <nguyen.jean@me.com>
2022-12-22 19:31:53 +0100

    decomment line

--- index.js
+++ index.js
@@ -7,14 +7,12 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const port = 3000;
-let folder = '/dist';
-let error404 = true;
 
-app.use(express.static(path.join(__dirname, folder)));
+app.use(express.static(path.join(__dirname, 'dist')));
 //render index.html page
-// app.get('/', (request, response) => {
-//   response.render('index.html');
-// });
+app.get('/', (request, response) => {
+  response.render('index.html');
+});
 
 if (typeof PhusionPassenger !== 'undefined') {
   app.listen('passenger');
